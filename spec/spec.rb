f8270c754d8ebb3860046b86e58be3fe5cc222ce
Shawna C Scott <shawnatyger@gmail.com>
2013-11-20 13:03:04 -0800

    Test for access to raw_customers.txt

--- spec/spec.rb
+++ spec/spec.rb
@@ -2,7 +2,11 @@ require './spec_helper.rb'
 
 describe 'prefixes histogram' do
 
-  it 'can access the raw_customers.txt file'
+  it 'can access the raw_customers.txt file' do
+    expect(File).to exist('../raw_customers.txt')
+  end
+
   it 'creates a hash of prefixes'
+
   it 'outputs a file of prefixes'
 end
