8167d058fbc0bb99f3e7d515240f1e802a67169e
Shawna C Scott <shawnatyger@gmail.com>
2013-11-20 12:55:06 -0800

    Set up testing structure and write pending tests

---
+++ spec/spec.rb
@@ -0,0 +1,8 @@
+require './spec_helper.rb'
+
+describe 'prefixes histogram' do
+
+  it 'can access the raw_customers.txt file'
+  it 'creates a hash of prefixes'
+  it 'outputs a file of prefixes'
+end
---
+++ spec/spec_helper.rb
@@ -0,0 +1,6 @@
+ENV['RACK_ENV'] = 'test'
+
+require 'rspec'
+require 'rspec/autorun'
+require 'rack/test'
+require '../analyze.rb'
