231cfa857ed50bf930b6b9827965cf88a32d6faf
Shawna C Scott <shawnatyger@gmail.com>
2013-12-20 17:03:49 -0800

    Refactor some tests

--- spec/spec_clean.rb
+++ spec/spec_clean.rb
@@ -2,20 +2,20 @@ require './spec_helper.rb'
 require '../clean.rb'
 
 def test_name
-  name_example = File.open'./input.txt', 'w'
+  name_example = File.open'../input.txt', 'w'
   name_example.puts 'Mr. Waylon Elisa Kozey-Kuhic III    776.953.9374'
   name_example.puts 'Tamara Witting V (738)753-1810 x561'
   name_example.close
-  expected_file = File.open './expected_output.csv', 'w'
+  expected_file = File.open '../expected_output.csv', 'w'
   expected_file.puts 'prefix,first_name,middle,last_name,suffix,
                       phone_number,phone_extension'
-  expected_file.puts 'Mr.,Waylon,Elisa,Kozey-Kuhic,III,+1.776.953.9374,'
-  expected_file.puts ',Tamara,,Witting,V,+1.738.753.1810,561'
+  expected_file.puts 'Mr.,Waylon,Elisa,Kozey-Kuhic,III,1.776.953.9374,'
+  expected_file.puts ',Tamara,,Witting,V,1.738.753.1810,561'
   expected_file.close
 end
 
 def delete_files
-  FileUtils.rm_r %w( ./input.txt ./expected_output.csv ./output.csv)
+  FileUtils.rm_r %w( ../input.txt ../expected_output.csv ../output.csv)
 end
 
 describe 'test the clean file output' do
@@ -25,14 +25,13 @@ describe 'test the clean file output' do
 
   it 'creates the output correctly' do
     test_name
-    `ruby ../clean.rb < input.txt > output.csv`
-    expect(FileUtils.identical? './expected_output.csv',
-                                './output.csv').to be_true
+    `ruby clean.rb -p prefix_words.txt -s suffix_words.txt -i input.txt -o output.csv`
+    expect(FileUtils.identical? '../expected_output.csv',
+                                '../output.csv').to be_true
   end
 
-  it 'creates the output correctly' do
-    test_name
-    `ruby ../clean.rb < input.txt > output.csv`
-    expect(name_parts).to be_true
-  end
+  # it 'creates the output correctly' do
+  #   test_name
+  #   `ruby ../clean.rb -p ../prefix_words.txt -s ../suffix_words.txt -i input.txt -o output.csv`
+  # end
 end
